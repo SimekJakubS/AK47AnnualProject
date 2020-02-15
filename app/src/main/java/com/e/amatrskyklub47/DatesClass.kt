50d8dd07dbfd4c592edb6ea6b59651f70506a44e
SimekJakubS <simek.jakubs@gmail.com>
2020-02-15 14:00:38 +0100

    Clear app

--- app/src/main/java/com/e/amatrskyklub47/DatesClass.kt
+++
@@ -1,9 +0,0 @@
-package com.e.amatrskyklub47
-
-import com.google.firebase.iid.FirebaseInstanceId
-
-class DatesClass(
-    //val idUdalost: String?,
-    val name: String,
-    val date: String
-    )
--- app/src/main/java/com/e/amatrskyklub47/NewDatabaseWrite.kt
+++ app/src/main/java/com/e/amatrskyklub47/NewDatabaseWrite.kt
@@ -9,44 +9,20 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
-class NewDatabaseWrite : AppCompatActivity() {
+class NewDatabaseWrite : AppCompatActivity()
+{
     private lateinit var nameWrite: EditText
     lateinit var dateWrite: EditText
     lateinit var databaseUpdateButton: Button
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.database_write)
 
         nameWrite = findViewById(R.id.nameWrite)
         dateWrite = findViewById(R.id.dateWrite)
         databaseUpdateButton = findViewById(R.id.databaseUpdateButton)
-
-        databaseUpdateButton.setOnClickListener {
-            saveSomething()
-        }
-
     }
 
-    private fun saveSomething() {
-        val name = nameWrite.text.toString().trim()
-        val date = dateWrite.text.toString().trim()
-
-        if (name.isEmpty()) {
-            nameWrite.error = "Zadajte meno"
-            return
-        }
-
-        val FirebaseDatabase = FirebaseDatabase.getInstance()
-        var databaseReference = FirebaseDatabase.getReference()
-        //Log.d("Logcat", "key je pushnuty")
-        databaseReference.child("udalosti").push().setValue("udalost")
-        
-
-        val udalost = DatesClass(idUdalost, name, date)
-
-        ref.child(idUdalost).setValue(udalost).addOnCompleteListener {
-            Toast.makeText(applicationContext, "DONE", Toast.LENGTH_SHORT).show()
-        }
-    }
 }
