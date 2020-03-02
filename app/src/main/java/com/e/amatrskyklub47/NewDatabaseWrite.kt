01bc15be7c99ef560b89397a0384e915f2b11d2f
SimekJakubS <simek.jakubs@gmail.com>
2020-03-02 09:10:18 +0100

    Total clear

--- app/src/main/java/com/e/amatrskyklub47/NewDatabaseWrite.kt
+++ app/src/main/java/com/e/amatrskyklub47/NewDatabaseWrite.kt
@@ -1,28 +1,14 @@
 package com.e.amatrskyklub47
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 
 class NewDatabaseWrite : AppCompatActivity()
 {
-    private lateinit var nameWrite: EditText
-    lateinit var dateWrite: EditText
-    lateinit var databaseUpdateButton: Button
-
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.database_write)
-
-        nameWrite = findViewById(R.id.nameWrite)
-        dateWrite = findViewById(R.id.dateWrite)
-        databaseUpdateButton = findViewById(R.id.databaseUpdateButton)
     }
 
 }
